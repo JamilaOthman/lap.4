e8c714079a72d9dfeaf01c530aa5fce45fca9ee4
JamilaOthman <mema20330@gmail.com>
2020-12-25 19:39:29 +0200

    lap8

---
+++ app2.js
@@ -0,0 +1,6 @@
+function greatuser() {
+  var yesorno= prompt("Please enter yes or no");
+  while (yesorno !=="yes"&&yesorno !=="no"){
+  gender = prompt("try agin: enter yes or no")
+  }
+}
--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
-alert("Hello!!we are happy to see you");
 var name = prompt("Please enter your name");
-confirm("Press contact us for help");
-var today= new Date();
-var hourNow = today.getHours();
-var greeting;
-if (hourNow > 18) {
-  greeting= 'Good evening!';
-} else if (hourNow > 12) {
-greeting = ' Good afternoon!';
-} else if (hourNow > 0) {
-greeting = 'Good morni ng!';
-} else {
-greeting = 'Welcome! ' ;
+function greatuser() {
+  var form;
+  var form= prompt("Please enter your gender f or m");
+  while (form !=="f"&&form !=="m"){
+  gender = prompt("try agin: enter f or m")
+  }
 }
-document.write( ' <h3>' +greeting + name+' </ h3> '); 
+greatuser();
+
+
+flower = prompt("how many kind of flower do you want");
+ for (var i=1; i< flower; i++){
+        document.write('<img width=200px hieght=100px src="../flowr.jpg">')
+ }
+
+
