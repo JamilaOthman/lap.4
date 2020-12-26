e528b6342924742bdd42334e03fab8c942f31eb4
JamilaOthman <mema20330@gmail.com>
2020-12-26 12:16:54 +0200

    lap8

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var name = prompt("Please enter your name");
- 
+ while(name ==""||name =="null"){
+   name = prompt("Please enter your name")
+ }
  function greatuser() {
   var form;
   var form= prompt("Please enter your gender f or m");
@@ -12,7 +14,7 @@ greatuser();
 
 flower = prompt("how many kind of flower do you want");
  for (var i=1; i<= flower; i++){
-        document.write('<img width=200 height=100 src="/../flowr.jpg">')
+        document.write('<img width=200 height=100 src="../flowr.jpg">')
  }
 
 
